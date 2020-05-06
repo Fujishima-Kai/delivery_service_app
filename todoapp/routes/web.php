23020a58b82bf26390c5d87125ba5dc7170efae8
Fujishima-Kai <fujishima.ki@gmail.com>
2020-05-06 23:30:10 +0900

    second commit

---
+++ todoapp/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/folders/create', 'FolderController@create')->name('folders.create');
+Route::post('/folders/create', 'FolderController@store')->name('folders.store');
+
+
+Route::get('/folders/{folder_id}/tasks', 'TaskController@index')->name('tasks.index');
+
+Route::get('/folders/{folder_id}/tasks/create', 'TaskController@create')->name('tasks.create');
+Route::post('/folders/{folder_id}/tasks/create', 'TaskController@store')->name('tasks.store');
+
+Route::get('/folders/{folder_id}/tasks/{task_id}/edit', 'TaskController@edit')->name('tasks.edit');
+Route::post('/folders/{folder_id}/tasks/{task_id}/edit', 'TaskController@update');
+
+Route::post('/folders/{folder_id}/tasks/{task}', 'TaskController@delete')->name('tasks.delete');
